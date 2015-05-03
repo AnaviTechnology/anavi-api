3b5813a8e9fa02386025706591d9171d05e9ec90
Leon Anavi <leon@anavi.org>
2015-05-03 23:23:47 +0300

    Hello Jadja with expressjs

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello Jadja!');
+});
+
+var server = app.listen(8090, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
