afe201837549fc34c8206778a896e268e1871967
Leon Anavi <leon@anavi.org>
2015-05-04 23:58:42 +0300

    minor change of the log message

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ var server = app.listen(8090, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Jadja-node listening at http://%s:%s', host, port);
 
 });
