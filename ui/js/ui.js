26318dc7f0d2ebbeb353d92cd6344c8c2b16b93e
Leon Anavi <leon@anavi.org>
2015-08-03 01:41:57 +0300

    External panel with options added
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- ui/js/ui.js
+++ ui/js/ui.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+  $( "body>[data-role='panel']" ).panel().enhanceWithin();
+
+  //$("[data-role=panel]").panel().enhanceWithin();
+
   $('#loginLogIn').bind('click', function(event) {
     $.mobile.pageContainer.pagecontainer("change", "#main");
   });
