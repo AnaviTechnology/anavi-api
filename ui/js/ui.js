5d97b9327a1a2d09dfdb67274cca3b6cb3704416
Leon Anavi <leon@anavi.org>
2015-08-06 02:41:47 +0300

    Dashboard added
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- ui/js/ui.js
+++ ui/js/ui.js
@@ -2,10 +2,8 @@ $(document).ready(function() {
 
   $( "body>[data-role='panel']" ).panel().enhanceWithin();
 
-  //$("[data-role=panel]").panel().enhanceWithin();
-
   $('#loginLogIn').bind('click', function(event) {
-    $.mobile.pageContainer.pagecontainer("change", "#devices");
+    $.mobile.pageContainer.pagecontainer("change", "#dashboard");
   });
 
 });
