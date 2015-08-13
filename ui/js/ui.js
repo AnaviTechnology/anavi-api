97bcbfeaf3ea019af7d4251ea98e756548c59b8d
Leon Anavi <leon@anavi.org>
2015-08-14 01:48:52 +0300

    Show charts per device
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- ui/js/ui.js
+++ ui/js/ui.js
@@ -6,22 +6,37 @@ $(document).ready(function() {
     $.mobile.pageContainer.pagecontainer("change", "#dashboard");
   });
 
+  $( "#device" ).on( "pagecreate", function( event, ui ) {
+    console.log('created');
+
+    $( "#deviceUsageLastWeek" ).on( "collapsibleexpand", function( event, ui ) {
+      console.log('deviceUsageLastWeek');
+      chartUsageLastWeek('deviceChartUsageLastWeek');
+    });
+
+    $( "#deviceUsageLastYear" ).on( "collapsibleexpand", function( event, ui ) {
+      console.log('deviceUsageLastYear');
+      chartUsageLastYear('deviceChartUsageLastYear');
+    });
+
+  } );
+
 });
 
 $(document).on('pagecontainershow', function(e, ui) {
     var pageId = $('body').pagecontainer('getActivePage').prop('id');
     if ('dashboard' === pageId) {
-      chartUsageLastWeek();
-      chartUsageLastYear();
+      chartUsageLastWeek('dashboardChartUsageLastWeek');
+      chartUsageLastYear('dashboardChartUsageLastYear');
     }
 });
 
-function chartUsageLastWeek() {
+function chartUsageLastWeek(chartId) {
   $.jqplot.config.enablePlugins = true;
   var s1 = [2, 6, 7, 10, 3, 5, 8];
   var ticks = ['7 Aug', '8 Aug', '9 Aug', '11 Aug', '12 Aug', '13 Aug', '14 Aug'];
 
-  plot1 = $.jqplot('dashboardChartUsageLastWeek', [s1], {
+  plot1 = $.jqplot(chartId, [s1], {
       // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
       animate: !$.jqplot.use_excanvas,
       seriesDefaults:{
@@ -39,13 +54,13 @@ function chartUsageLastWeek() {
 
 }
 
-function chartUsageLastYear() {
+function chartUsageLastYear(chartId) {
   $.jqplot.config.enablePlugins = true;
   var s1 = [200, 250, 300, 360, 350, 425, 375, 300, 150, 250, 260, 150];
   var ticks = ['Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb',
               'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'];
 
-  plot1 = $.jqplot('dashboardChartUsageLastYear', [s1], {
+  plot1 = $.jqplot(chartId, [s1], {
       // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
       animate: !$.jqplot.use_excanvas,
       seriesDefaults:{
