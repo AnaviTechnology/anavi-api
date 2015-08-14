41f3eadc556f86061206b4b2406b82f9b536c018
Leon Anavi <leon@anavi.org>
2015-08-15 00:32:02 +0300

    Bug fixes related to charts
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- ui/js/ui.js
+++ ui/js/ui.js
@@ -7,72 +7,103 @@ $(document).ready(function() {
   });
 
   $( "#device" ).on( "pagecreate", function( event, ui ) {
-    console.log('created');
 
     $( "#deviceUsageLastWeek" ).on( "collapsibleexpand", function( event, ui ) {
-      console.log('deviceUsageLastWeek');
-      chartUsageLastWeek('deviceChartUsageLastWeek');
+      charts.chartUsageLastWeek('deviceChartUsageLastWeek');
     });
 
     $( "#deviceUsageLastYear" ).on( "collapsibleexpand", function( event, ui ) {
-      console.log('deviceUsageLastYear');
-      chartUsageLastYear('deviceChartUsageLastYear');
+      charts.chartUsageLastYear('deviceChartUsageLastYear');
     });
 
   } );
 
 });
 
+
+$(document).on('pagecontainerbeforeshow', function(e, ui) {
+  var pageId = $('body').pagecontainer('getActivePage').prop('id');
+  if ('dashboard' === pageId) {
+    charts.destroyCharts();
+  }
+  else if ('device' === pageId) {
+    charts.destroyCharts();
+  }
+});
+
 $(document).on('pagecontainershow', function(e, ui) {
     var pageId = $('body').pagecontainer('getActivePage').prop('id');
     if ('dashboard' === pageId) {
-      chartUsageLastWeek('dashboardChartUsageLastWeek');
-      chartUsageLastYear('dashboardChartUsageLastYear');
+      charts.chartUsageLastWeek('dashboardChartUsageLastWeek');
+      charts.chartUsageLastYear('dashboardChartUsageLastYear');
     }
 });
 
-function chartUsageLastWeek(chartId) {
-  $.jqplot.config.enablePlugins = true;
-  var s1 = [2, 6, 7, 10, 3, 5, 8];
-  var ticks = ['7 Aug', '8 Aug', '9 Aug', '11 Aug', '12 Aug', '13 Aug', '14 Aug'];
+var charts = {
+  chartWeek: null,
 
-  plot1 = $.jqplot(chartId, [s1], {
+  chartYear: null,
+
+  destroyCharts: function() {
+    if (this.chartWeek !== null) {
+      this.chartWeek.destroy();
+    }
+    if (this.chartYear !== null) {
+      this.chartYear.destroy();
+    }
+  },
+
+  chartUsageLastWeek: function(chartId) {
+    $.jqplot.config.enablePlugins = true;
+    var s1 = [2, 6, 7, 10, 3, 5, 8];
+    var ticks = ['7 Aug', '8 Aug', '9 Aug', '11 Aug', '12 Aug', '13 Aug', '14 Aug'];
+
+    if (this.chartWeek !== null) {
+      this.chartWeek.destroy();
+    }
+
+    this.chartWeek = $.jqplot(chartId, [s1], {
       // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
       animate: !$.jqplot.use_excanvas,
       seriesDefaults:{
-          renderer:$.jqplot.BarRenderer,
-          pointLabels: { show: true }
+        renderer:$.jqplot.BarRenderer,
+        pointLabels: { show: true }
       },
       axes: {
-          xaxis: {
-              renderer: $.jqplot.CategoryAxisRenderer,
-              ticks: ticks
-          }
+        xaxis: {
+          renderer: $.jqplot.CategoryAxisRenderer,
+          ticks: ticks
+        }
       },
       highlighter: { show: false }
-  });
+    });
 
-}
+  },
 
-function chartUsageLastYear(chartId) {
-  $.jqplot.config.enablePlugins = true;
-  var s1 = [200, 250, 300, 360, 350, 425, 375, 300, 150, 250, 260, 150];
-  var ticks = ['Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb',
-              'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'];
+  chartUsageLastYear : function(chartId) {
+    $.jqplot.config.enablePlugins = true;
+    var s1 = [200, 250, 300, 360, 350, 425, 375, 300, 150, 250, 260, 150];
+    var ticks = ['Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb',
+                'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'];
 
-  plot1 = $.jqplot(chartId, [s1], {
+    if (this.chartYear !== null) {
+      this.chartYear.destroy();
+    }
+
+    this.chartYear = $.jqplot(chartId, [s1], {
       // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
       animate: !$.jqplot.use_excanvas,
       seriesDefaults:{
-          renderer:$.jqplot.BarRenderer,
-          pointLabels: { show: true }
+        renderer:$.jqplot.BarRenderer,
+        pointLabels: { show: true }
       },
       axes: {
-          xaxis: {
-              renderer: $.jqplot.CategoryAxisRenderer,
-              ticks: ticks
-          }
+        xaxis: {
+            renderer: $.jqplot.CategoryAxisRenderer,
+            ticks: ticks
+        }
       },
       highlighter: { show: false }
-  });
-}
+    });
+  }
+};
