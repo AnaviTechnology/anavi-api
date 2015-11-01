077e1e6b87f958544c8e15709542983737a810d8
Leon Anavi <leon@anavi.org>
2015-11-02 01:30:33 +0200

    Database: devices
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- sql/data.sql
+++ sql/data.sql
@@ -1,12 +1,48 @@
 USE `rabbitpi`;
 
-INSERT INTO `users` (`user_id`, `user_username`, `user_password`, `user_email`, `user_display_name`, `user_display_surname`) VALUES
-(1, 'demo', '89e495e7941cf9e40e6980d14a16bf023ccd4c91', 'demo@example.com', 'John', 'Doe'),
-(2, 'admin', '89e495e7941cf9e40e6980d14a16bf023ccd4c91', 'admin@example.com', 'Jane', 'Doe');
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rabbitpi`
+--
+
+--
+-- Dumping data for table `organizations`
+--
 
 INSERT INTO `organizations` (`organization_id`, `organization_name`) VALUES
 (1, 'demo');
 
+--
+-- Dumping data for table `organizations_users`
+--
+
 INSERT INTO `organizations_users` (`ou_id`, `ou_organization_id`, `ou_user_id`) VALUES
 (1, 1, 1),
 (2, 1, 2);
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`settings_id`, `settings_user_id`, `settings_type`, `settings_value`) VALUES
+(11, 1, 'home', 'pageDevices');
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_username`, `user_password`, `user_email`, `user_display_name`, `user_display_surname`) VALUES
+(1, 'demo', '89e495e7941cf9e40e6980d14a16bf023ccd4c91', 'demo@example.com', 'John', 'Doe'),
+(2, 'admin', '89e495e7941cf9e40e6980d14a16bf023ccd4c91', 'admin@example.com', 'Jane', 'Doe');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- sql/db.sql
+++ sql/db.sql
@@ -15,6 +15,77 @@ USE `rabbitpi`;
 
 -- --------------------------------------------------------
 
+-- phpMyAdmin SQL Dump
+-- version 4.4.13.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 02, 2015 at 01:27 AM
+-- Server version: 5.6.27-0ubuntu1
+-- PHP Version: 5.6.11-1ubuntu3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rabbitpi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `devices`
+--
+
+CREATE TABLE IF NOT EXISTS `devices` (
+  `device_id` int(20) NOT NULL,
+  `device_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `device_type_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `device_types`
+--
+
+CREATE TABLE IF NOT EXISTS `device_types` (
+  `device_type_id` int(5) NOT NULL,
+  `device_type` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `device_type_features`
+--
+
+CREATE TABLE IF NOT EXISTS `device_type_features` (
+  `dtf_id` int(12) NOT NULL,
+  `dtf_type_id` int(5) NOT NULL,
+  `dtf_feature_id` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `features`
+--
+
+CREATE TABLE IF NOT EXISTS `features` (
+  `feature_id` int(6) NOT NULL,
+  `feature_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `feature_topic` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `organizations`
 --
@@ -22,6 +93,18 @@ USE `rabbitpi`;
 CREATE TABLE IF NOT EXISTS `organizations` (
   `organization_id` int(6) NOT NULL,
   `organization_name` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organizations_devices`
+--
+
+CREATE TABLE IF NOT EXISTS `organizations_devices` (
+  `od_id` int(20) NOT NULL,
+  `od_organization_id` int(6) NOT NULL,
+  `od_device_id` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -47,7 +130,9 @@ CREATE TABLE IF NOT EXISTS `settings` (
   `settings_user_id` int(12) NOT NULL,
   `settings_type` enum('home') COLLATE utf8_unicode_ci NOT NULL,
   `settings_value` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
@@ -67,11 +152,47 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 --
+-- Indexes for table `devices`
+--
+ALTER TABLE `devices`
+  ADD PRIMARY KEY (`device_id`),
+  ADD UNIQUE KEY `device_name` (`device_name`);
+
+--
+-- Indexes for table `device_types`
+--
+ALTER TABLE `device_types`
+  ADD PRIMARY KEY (`device_type_id`),
+  ADD UNIQUE KEY `device_type` (`device_type`);
+
+--
+-- Indexes for table `device_type_features`
+--
+ALTER TABLE `device_type_features`
+  ADD PRIMARY KEY (`dtf_id`),
+  ADD UNIQUE KEY `dtf_type_id` (`dtf_type_id`,`dtf_feature_id`);
+
+--
+-- Indexes for table `features`
+--
+ALTER TABLE `features`
+  ADD PRIMARY KEY (`feature_id`),
+  ADD UNIQUE KEY `feature_name` (`feature_name`),
+  ADD UNIQUE KEY `feature_topic` (`feature_topic`);
+
+--
 -- Indexes for table `organizations`
 --
 ALTER TABLE `organizations`
   ADD PRIMARY KEY (`organization_id`),
   ADD UNIQUE KEY `organization_name` (`organization_name`);
+
+--
+-- Indexes for table `organizations_devices`
+--
+ALTER TABLE `organizations_devices`
+  ADD PRIMARY KEY (`od_id`),
+  ADD UNIQUE KEY `od_organization_id` (`od_organization_id`,`od_device_id`);
 
 --
 -- Indexes for table `organizations_users`
@@ -100,22 +221,45 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `devices`
+--
+ALTER TABLE `devices`
+  MODIFY `device_id` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `device_types`
+--
+ALTER TABLE `device_types`
+  MODIFY `device_type_id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `device_type_features`
+--
+ALTER TABLE `device_type_features`
+  MODIFY `dtf_id` int(12) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `features`
+--
+ALTER TABLE `features`
+  MODIFY `feature_id` int(6) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `organizations`
 --
 ALTER TABLE `organizations`
   MODIFY `organization_id` int(6) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `organizations_devices`
+--
+ALTER TABLE `organizations_devices`
+  MODIFY `od_id` int(20) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `organizations_users`
 --
 ALTER TABLE `organizations_users`
   MODIFY `ou_id` int(12) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
   MODIFY `settings_id` int(20) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT for table `users`
 --
